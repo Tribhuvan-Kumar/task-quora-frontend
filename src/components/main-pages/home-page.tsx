62b6e5a4411ef5604ea1c884f11134539257dc82
Tribhuvan <acr.trii@gmail.com>
2024-01-14 14:49:09 +0530

    removed toast if direct accessing homepage without login

--- src/components/main-pages/home-page.tsx
+++ src/components/main-pages/home-page.tsx
@@ -2,12 +2,10 @@ import { useEffect, useState } from "react";
 import HomePageAllPost from "../home-page-components/home-all-post";
 import HomeNavbar from "../home-page-components/home-navbar";
 import { useNavigate } from "react-router-dom";
-import { useToast } from "@/components/ui/use-toast";
 
 const HomePage = () => {
   const [isLogin, setIsLogin] = useState<boolean>(true);
   const navigate = useNavigate();
-  const { toast } = useToast();
 
   useEffect(() => {
     const cookies = document.cookie;
@@ -17,11 +15,6 @@ const HomePage = () => {
     } else {
       setIsLogin(false);
       navigate("/login");
-
-      toast({
-        variant: "destructive",
-        description: "Uh oh! Unauthorized request.",
-      });
     }
   }, []);
 
